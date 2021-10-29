01e7e1ad7eed156ef06f6de1ce48e4833b9f85f7
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-10-29 18:24:15 +0200

    Store DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 
 // NgRx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { counterReducer } from './contador/contador.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
-
 
 @NgModule({
   declarations: [
@@ -22,6 +24,10 @@ import { NietoComponent } from './contador/nieto/nieto.component';
     AppRoutingModule,
     StoreModule.forRoot({
       counter: counterReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
     })
   ],
   providers: [],

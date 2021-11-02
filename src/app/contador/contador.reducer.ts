d358011b934b3351c8b064ad681ad357a61d0fc0
Jose Carlos Jaramago Reyes <jose.jaramago@escenit.com>
2021-11-02 11:34:12 +0100

    create reducer

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,13 +1,25 @@
-import { Action } from "@ngrx/store";
+import { createReducer, on, Action } from '@ngrx/store';
 import { increment, decrement } from './contador.actions';
 
-export function counterReducer(state: number = 10, action: Action) {
-    switch (action.type) {
-        case increment.type:
-            return state + 1;
-        case decrement.type:
-            return state - 1;
-        default:
-            return state;
-    }
+// export function counterReducer(state: number = 10, action: Action) {
+//     switch (action.type) {
+//         case increment.type:
+//             return state + 1;
+//         case decrement.type:
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// }
+
+export const initialState = 20;
+
+const _counterReducer = createReducer(
+    initialState,
+    on(increment, state => state + 1),
+    on(decrement, state => state - 1)
+)
+
+export function counterReducer(state: number = initialState, action: Action) {
+    return _counterReducer(state, action)
 }
